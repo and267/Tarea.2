95d162129ce7b64931510e354404d2a65d33f33b
and267 <148586602+and267@users.noreply.github.com>
2023-10-20 20:25:03 -0300

    Desarrollo superclase Moneda

--- Tarea-2/src/main/java/org/example/Moneda.java
+++ Tarea-2/src/main/java/org/example/Moneda.java
@@ -1,4 +1,19 @@
 package org.example;
 
-public class Moneda {
+abstract class Moneda implements Comparable<Moneda>{
+
+    protected int valor;
+    protected int serie;
+
+    public Moneda(){
+    }
+    public Moneda getSerie(){
+        return this;
+    }
+
+    public abstract int getValor();
+
+    public int compareTo(Moneda m) {
+        return (this.getValor() - m.getValor());
+    }
 }
