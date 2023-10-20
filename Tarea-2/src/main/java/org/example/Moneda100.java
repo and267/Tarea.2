1296c3e5809f88de739debd718a2fbb025adc5e3
and267 <148586602+and267@users.noreply.github.com>
2023-10-20 20:27:15 -0300

    Desarrollo subclase Moneda100

--- Tarea-2/src/main/java/org/example/Moneda100.java
+++ Tarea-2/src/main/java/org/example/Moneda100.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class Moneda100 {
+/**
+ * Moneda con valor 100
+ */
+class Moneda100 extends Moneda{
+    public Moneda100(){
+        super();
+        super.valor = 100;
+    }
+    public int getValor(){
+        return super.valor;
+    }
 }
