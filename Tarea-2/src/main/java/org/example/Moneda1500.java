331a8e138cc00521e16196b78b0edb591291dbb4
and267 <148586602+and267@users.noreply.github.com>
2023-10-20 20:31:13 -0300

    Desarrollo subclase Moneda1500

--- Tarea-2/src/main/java/org/example/Moneda1500.java
+++ Tarea-2/src/main/java/org/example/Moneda1500.java
@@ -1,4 +1,14 @@
 package org.example;
 
-public class Moneda1500 {
+/**
+ * Moneda de 1500
+ */
+class Moneda1500 extends Moneda{
+    public Moneda1500(){
+        super();
+        super.valor = 1500;
+    }
+    public int getValor(){
+        return super.valor;
+    }
 }
