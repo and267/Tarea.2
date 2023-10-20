109ed924c71032991e4b726a834ddd6eb1782f50
and267 <148586602+and267@users.noreply.github.com>
2023-10-20 20:22:41 -0300

    Desarrollo subclase Super8

--- Tarea-2/src/main/java/org/example/Super8.java
+++ Tarea-2/src/main/java/org/example/Super8.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class Super8 {
+public class Super8 extends Dulce {
+
+    public Super8(int s){
+        super(s);
+    }
+    
+    public String consumir(){
+        return "super8";
+    }
 }
