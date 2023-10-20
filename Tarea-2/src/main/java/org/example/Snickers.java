2385c839f243a00fa21c9037c6f6595e513bb6d4
and267 <148586602+and267@users.noreply.github.com>
2023-10-20 20:20:31 -0300

    Desarrollo subclase Snickers

--- Tarea-2/src/main/java/org/example/Snickers.java
+++ Tarea-2/src/main/java/org/example/Snickers.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class Snickers {
+public class Snickers extends Dulce{
+
+    public Snickers(int s){
+        super(s);
+    }
+
+    public String consumir(){
+        return "snickers";
+    }
 }
