93c17ea64c5f2c7ec36638a984d30496fdd208ba
and267 <148586602+and267@users.noreply.github.com>
2023-10-20 20:33:28 -0300

    Desarrollo clase Deposito
    Se agrega su respectiva documentación.

--- Tarea-2/src/main/java/org/example/Deposito.java
+++ Tarea-2/src/main/java/org/example/Deposito.java
@@ -1,4 +1,34 @@
 package org.example;
 
-public class Deposito {
+import java.util.ArrayList;
+
+/** 
+ * Un deposito de tipo generico que almacenara los productos (bebidas y dulces).
+ * @author Darwin Albornoz
+ * @version versión 1, 20 de octubre de 2023
+ */
+class Deposito<T>{
+    private ArrayList<T> dep;
+    
+    //Metodo constructor clase Deposito<T>, que crea un objeto de tipo ArrayList<T> para el funcionamiento del deposito.
+    public Deposito(){
+        dep = new ArrayList<T>();
+    }
+    
+    //Metodo addObj, agrega elementos en el deposito.
+    public void addObj(T bar){
+        dep.add(bar);
+    }
+
+    //Metodo getObj, retira elementos del deposito.
+    public T getObj(){ 
+        if (dep.size() == 0){
+            return null;
+        }
+        else{
+            T ret = dep.get(0);
+            dep.remove(0);
+            return ret;
+        }
+    }
 }
